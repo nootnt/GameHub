c6f315c6597ec454c54e3aa66a0d68e5360fb315
M2I0R0O5 <149790660+M2I0R0O5@users.noreply.github.com>
2023-12-04 10:01:51 +0400

    Update c4.py
    this too

--- src/c4.py
+++ src/c4.py
@@ -65,6 +65,64 @@ while running:
                         )
                         screen.blit(label, (40, 10))
                         game_over = True
+         def drop_piece(board, row, col, piece):
+    board[row][col] = piece
+
+
+def is_valid_location(board, col):
+    return board[ROW_COUNT - 1][col] == 0
+
+
+def get_next_open_row(board, col):
+    for r in range(ROW_COUNT):
+        if board[r][col] == 0:
+            return r
+
+
+def winning_move(board, piece):
+    # Check horizontal locations
+    for c in range(COLUMN_COUNT - 3):
+        for r in range(ROW_COUNT):
+            if (
+                board[r][c] == piece
+                and board[r][c + 1] == piece
+                and board[r][c + 2] == piece
+                and board[r][c + 3] == piece
+            ):
+                return True
+
+    # Check vertical locations
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT - 3):
+            if (
+                board[r][c] == piece
+                and board[r + 1][c] == piece
+                and board[r + 2][c] == piece
+                and board[r + 3][c] == piece
+            ):
+                return True
+
+    # Check positively sloped diagonals
+    for c in range(COLUMN_COUNT - 3):
+        for r in range(ROW_COUNT - 3):
+            if (
+                board[r][c] == piece
+                and board[r + 1][c + 1] == piece
+                and board[r + 2][c + 2] == piece
+                and board[r + 3][c + 3] == piece
+            ):
+                return True
+
+    # Check negatively sloped diagonals
+    for c in range(COLUMN_COUNT - 3):
+        for r in range(3, ROW_COUNT):
+            if (
+                board[r][c] == piece
+                and board[r - 1][c + 1] == piece
+                and board[r - 2][c + 2] == piece
+                and board[r - 3][c + 3] == piece
+            ):
+                return True
 
 
 pygame.quit()
