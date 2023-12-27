0797f71b4887ef2635a61a8ef40968b3ee5e76c3
M2I0R0O5 <149790660+M2I0R0O5@users.noreply.github.com>
2023-12-28 03:29:39 +0400

    tictactoe
    needs some bug fixes
    (damn I am actually doing stuff)

--- src/ttt.py
+++ src/ttt.py
@@ -1,41 +1,123 @@
 import pygame
-screen = pygame.display.set_mode((1280, 720))
+import sys
+
+# Initialize Pygame
 pygame.init()
 
-clock = pygame.time.Clock()
-running = True
-dt = 0
+# Set up the game window
+width, height = 600, 600
+win = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption("GameHub_Tic Tac Toe")
 
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+# Set up the game board
+board = [['', '', ''], ['', '', ''], ['', '', '']]
+player_turn = 'X'
+last_player = 'O'
 
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
+# Display of player wining
+myfont = pygame.font.SysFont("monospace", 130)
+
+# Draw the game board
+def draw_board():
+    win.fill("black")
+    pygame.draw.line(win, "dark grey", (width // 3, 60), (width // 3, height + 60), 5)
+    pygame.draw.line(win, "dark grey", (2 * width // 3, 60), (2 * width // 3, height + 60), 5)
+    pygame.draw.line(win, "dark grey", (0, height // 3 + 60), (width, height // 3 + 60), 5)
+    pygame.draw.line(win, "dark grey", (0, 2 * height // 3 + 60), (width, 2 * height // 3 + 60), 5)
+
+    for row in range(3):
+        for col in range(3):
+            if board[row][col] == 'X':
+                draw_x(row, col)
+            elif board[row][col] == 'O':
+                draw_o(row, col)
+
+# Draw X at a given row and column
+def draw_x(row, col):
+    pygame.draw.line(win, "orange", (col * width // 3, row * height // 3 + 60), ((col + 1) * width // 3, (row + 1) * height // 3 + 60), 5)
+    pygame.draw.line(win, "orange", ((col + 1) * width // 3, row * height // 3 + 60), (col * width // 3, (row + 1) * height // 3 + 60), 5)
+
+# Draw O at a given row and column
+def draw_o(row, col):
+    pygame.draw.circle(win, "cyan", ((col * width // 3) + width // 6, (row * height // 3) + height // 6 + 60), width // 6, 5)
+
+# Check if a player has won
+def check_winner():
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2] != '':
+            return True
+        if board[0][i] == board[1][i] == board[2][i] != '':
+            return True
+    if board[0][0] == board[1][1] == board[2][2] != '':
+        return True
+    if board[0][2] == board[1][1] == board[2][0] != '':
+        return True
+    return False
+
+# Check if the board is full (a tie)
+def check_tie():
+    for row in board:
+        for cell in row:
+            if cell == '':
+                return False
+    return True
+
+# Main game loop
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            pygame.quit()
+            sys.exit()
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("white")
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            # Get the row and column clicked
+            mouseX, mouseY = pygame.mouse.get_pos()
+            clicked_row = mouseY // (height // 3 + 60)
+            clicked_col = mouseX // (width // 3)
+       
+        
 
-    pygame.draw.circle(screen, "red", player_pos, 40)
+            # Make a move if the clicked cell is empty
+            if board[clicked_row][clicked_col] == '':
+                board[clicked_row][clicked_col] = player_turn
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
+                if player_turn == 'O':
+                    player_turn = 'X'
+                else:
+                    player_turn = 'O'
+                
+                if last_player == 'O':
+                    last_player = 'X'
+                else:
+                    last_player = 'O'
 
-    # flip() the display to put your work on screen
-    pygame.display.flip()
+        
 
-    # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
-    dt = clock.tick(60) / 1000
+    draw_board()
+    pygame.display.update()
 
-pygame.quit()
+    # Check for a winner or tie
+    if check_winner():
+        label_player = myfont.render(f"Player {last_player}", 1, "dark grey")
+        label_wins = myfont.render("Wins!!", 1, "dark grey")
+        win.blit(label_player, (630, 200))
+        win.blit(label_wins, (730, 350))
+
+        pygame.display.update()        
+        
+        print(f"Player {player_turn} wins!")
+        pygame.time.delay(3000)
+        pygame.quit()
+        sys.exit()
+    elif check_tie():
+        label_draw = myfont.render("Skill", 1, "dark grey")
+        label_draw2 = myfont.render("Issues??", 1, "dark grey")
+        win.blit(label_draw, (730, 200))
+        win.blit(label_draw2, (650, 350))  
+
+        pygame.display.update()  
+
+        print("It's a tie!")
+        pygame.time.delay(3000)
+        pygame.quit()
+        sys.exit()
