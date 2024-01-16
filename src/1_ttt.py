eb47b9869ec696d3bdcbe8a1e4dda674e763b85a
noot <DefenestrationUsurper@proton.me>
2024-01-17 01:51:26 +0400

    ttt fix
    - colors
    - comments
    + icon

--- src/1_ttt.py
+++ src/1_ttt.py
@@ -7,23 +7,27 @@ pygame.init()
 # Set up the game window
 width, height = 600, 600
 win = pygame.display.set_mode((1280, 720))
-pygame.display.set_caption("GameHub_Tic Tac Toe")
+pygame.display.set_caption("GameHub(tm) Tic Tac Toe")
+
+# set window icon
+ico = pygame.image.load("res/GH.png")
+pygame.display.set_icon(ico)
+
+# Define font var
+myfont = pygame.font.SysFont("monospace", 130)
 
 # Set up the game board
 board = [['', '', ''], ['', '', ''], ['', '', '']]
 player_turn = 'X'
 last_player = 'O'
 
-# Display of player wining
-myfont = pygame.font.SysFont("monospace", 130)
-
 # Draw the game board
 def draw_board():
-    win.fill("black")
-    pygame.draw.line(win, "dark grey", (width // 3, 60), (width // 3, height + 60), 5)
-    pygame.draw.line(win, "dark grey", (2 * width // 3, 60), (2 * width // 3, height + 60), 5)
-    pygame.draw.line(win, "dark grey", (0, height // 3 + 60), (width, height // 3 + 60), 5)
-    pygame.draw.line(win, "dark grey", (0, 2 * height // 3 + 60), (width, 2 * height // 3 + 60), 5)
+    win.fill("#0D0D0D")
+    pygame.draw.line(win, "#ffffff", (width // 3, 60), (width // 3, height + 60), 5)
+    pygame.draw.line(win, "#ffffff", (2 * width // 3, 60), (2 * width // 3, height + 60), 5)
+    pygame.draw.line(win, "#ffffff", (0, height // 3 + 60), (width, height // 3 + 60), 5)
+    pygame.draw.line(win, "#ffffff", (0, 2 * height // 3 + 60), (width, 2 * height // 3 + 60), 5)
 
     for row in range(3):
         for col in range(3):
@@ -34,12 +38,12 @@ def draw_board():
 
 # Draw X at a given row and column
 def draw_x(row, col):
-    pygame.draw.line(win, "orange", (col * width // 3, row * height // 3 + 60), ((col + 1) * width // 3, (row + 1) * height // 3 + 60), 5)
-    pygame.draw.line(win, "orange", ((col + 1) * width // 3, row * height // 3 + 60), (col * width // 3, (row + 1) * height // 3 + 60), 5)
+    pygame.draw.line(win, "#F66600", (col * width // 3, row * height // 3 + 60), ((col + 1) * width // 3, (row + 1) * height // 3 + 60), 5)
+    pygame.draw.line(win, "#F66600", ((col + 1) * width // 3, row * height // 3 + 60), (col * width // 3, (row + 1) * height // 3 + 60), 5)
 
 # Draw O at a given row and column
 def draw_o(row, col):
-    pygame.draw.circle(win, "cyan", ((col * width // 3) + width // 6, (row * height // 3) + height // 6 + 60), width // 6, 5)
+    pygame.draw.circle(win, "#00A1EA", ((col * width // 3) + width // 6, (row * height // 3) + height // 6 + 60), width // 6, 5)
 
 # Check if a player has won
 def check_winner():
@@ -70,14 +74,14 @@ while True:
             sys.exit()
 
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            # Get the row and column clicked
+            # Find imput row/collum
             mouseX, mouseY = pygame.mouse.get_pos()
             clicked_row = mouseY // (height // 3 + 60)
             clicked_col = mouseX // (width // 3)
        
         
 
-            # Make a move if the clicked cell is empty
+            # Continiue if the clicked cell is empty
             if board[clicked_row][clicked_col] == '':
                 board[clicked_row][clicked_col] = player_turn
 
@@ -98,8 +102,8 @@ while True:
 
     # Check for a winner or tie
     if check_winner():
-        label_player = myfont.render(f"Player {last_player}", 1, "dark grey")
-        label_wins = myfont.render("Wins!!", 1, "dark grey")
+        label_player = myfont.render(f"Player {last_player}", 1, "#ffffff")
+        label_wins = myfont.render("Wins!!", 1, "#ffffff")
         win.blit(label_player, (630, 200))
         win.blit(label_wins, (730, 350))
 
@@ -110,8 +114,8 @@ while True:
         pygame.quit()
         sys.exit()
     elif check_tie():
-        label_draw = myfont.render("Skill", 1, "dark grey")
-        label_draw2 = myfont.render("Issues??", 1, "dark grey")
+        label_draw = myfont.render("Skill", 1, "#ffffff")
+        label_draw2 = myfont.render("Issues??", 1, "#ffffff")
         win.blit(label_draw, (730, 200))
         win.blit(label_draw2, (650, 350))  
 
